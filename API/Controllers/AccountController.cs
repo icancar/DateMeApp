9d75d4a3e56f3d9d9aadb19784b294f916c82d71
Igor Cancar <icancar99@gmail.com>
2024-03-27 23:51:53 +0100

    Add Data transfer object for registration

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,8 +5,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -20,18 +22,25 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password) {
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO) {
+            
+            if(await UserExists(registerDTO.UserName)) return BadRequest("User already exists!");
+            
             using var hmac = new HMACSHA512();
 
             var user = new AppUser {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDTO.UserName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
             };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+
+        private async Task<bool> UserExists(string username) {
+            return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
         }
     }
 }
