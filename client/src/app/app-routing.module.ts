6fa0f3136f853cd01eb0292106f6d2255de61b24
Igor Cancar <icancar99@gmail.com>
2024-04-04 22:45:15 +0200

    Add router guards

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'member/:id', component: MemberDetailComponent},
-  {path: 'members', component: MemberListComponent},
-  {path: 'messages', component: MessagesComponent},
+  {path: 'member/:id', component: MemberDetailComponent, canActivate:[authGuard]},
+  {path: 'members', component: MemberListComponent, canActivate:[authGuard]},
+  {path: 'messages', component: MessagesComponent, canActivate:[authGuard]},
   {path: '**', component: HomeComponent}
 ];
 
