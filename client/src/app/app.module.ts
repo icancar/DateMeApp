cfe17d6d7e660bde96b013be010a9bb9b2b526be
Igor Cancar <icancar99@gmail.com>
2024-04-05 23:25:01 +0200

    Remove not used import

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,7 +14,6 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared/shared.module';
 
 @NgModule({
