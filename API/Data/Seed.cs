b3a9ece1ee854923b9496e81e43df26d6635e68e
Igor Cancar <icancar99@gmail.com>
2024-05-23 16:11:40 +0200

    Seed data at application start

---
+++ API/Data/Seed.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public static class Seed
+{
+    public static async Task SeedUsers(DataContext context) {
+        if(await context.Users.CountAsync() > 0) return;
+
+        var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+        foreach(var user in users){
+            var hmac = new HMACSHA512();
+
+            user.UserName = user.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("P@ssw0rd"));
+            user.PasswordSalt = hmac.Key;
+
+            context.Users.Add(user);
+
+            await context.SaveChangesAsync();
+
+        }
+    }
+}
